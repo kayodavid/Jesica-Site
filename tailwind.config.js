d9f5573653bf4fa13cd86b4ebcdffb043d137e5b
kayodavid <kayodavids@gmail.com>
2025-07-27 12:04:42 -0300

    edição de estilo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: [
     "./index.html",
+    "./agemdar.html", // Adicione esta linha
+    // ou, para ser mais abrangente:
+    // "./*.html",
     "./**/*.{js,ts,jsx,tsx}",
     "./movie-details.html",
   ],
@@ -11,8 +15,11 @@ export default {
         primary: '#3490dc',
         secondary: '#ffed4a',
         danger: '#e3342f',
+        bacground: '#E2E9E6',
+        bacground_2: '#EDF5F2',
+        black_2: '#262626',
       },
     },
   },
   plugins: [],
-} 
+}
