d1e4cacd6da4395aa8ab4c02468a52863ea6c557
kayodavid <kayodavids@gmail.com>
2025-07-28 15:28:06 -0300

    Fix background color typo and update text colors for better readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#3490dc',
-        secondary: '#ffed4a',
-        danger: '#e3342f',
         bacground: '#E2E9E6',
         bacground_2: '#EDF5F2',
         black_2: '#262626',
