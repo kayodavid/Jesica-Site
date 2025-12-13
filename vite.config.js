b4c0d2cdb55f3c6e68f8b1bdb977de0d10e91ca5
kayodavid <kayodavids@gmail.com>
2025-12-13 17:38:16 -0300

    🐛vit.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        agendamento: 'agemdar.html', 
+        agendamento: 'agendamento.html', 
       },
     },
   },
