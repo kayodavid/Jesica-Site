36918b703e7e02263b798edd4bf93d0ff1a422ba
kayodavid <kayodavids@gmail.com>
2026-09-08 13:01:19 -0300

    fix: rename duplicate selected variable

---
+++ fix2.cjs
@@ -0,0 +1,13 @@
+﻿const fs = require('fs');
+const path = 'c:/Users/kayod/Downloads/projeto_jesica_atualizado/admin-espaco-paciente.html';
+let html = fs.readFileSync(path, 'utf8');
+const lines = html.split('\n');
+
+const start = lines.findIndex(l => l.includes('if (!selectedIds || selectedIds.size === 0) {'));
+if (start > -1) {
+  lines[start + 3] = '        const selectedList = getSelectedQuestions();';
+  lines[start + 4] = '        if (selectedList.length === 0) {';
+  lines[start + 8] = '        const selected = getSelectedQuestions();';
+  fs.writeFileSync(path, lines.join('\n'), 'utf8');
+  console.log('Fixed duplicate by renaming the first one to selectedList');
+}
---
+++ fix4.cjs
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+const path = 'c:/Users/kayod/Downloads/projeto_jesica_atualizado/admin-espaco-paciente.html';
+let html = fs.readFileSync(path, 'utf8');
+
+html = html.replace('const selected = getSelectedQuestions();\n        if (selected.length === 0) {', 'const selectedList = getSelectedQuestions();\n        if (selectedList.length === 0) {');
+
+fs.writeFileSync(path, html, 'utf8');
+console.log('Fixed selected duplicate');
